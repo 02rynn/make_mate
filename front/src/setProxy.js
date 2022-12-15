7a66f30e3ceed34f95a45023568facbc3aa835dc
Charm-Star <zxzx05050@naver.com>
2022-12-15 16:43:01 +0900

    프론트엔드 백엔드 연결 테스트

---
+++ front/src/setProxy.js
@@ -0,0 +1,15 @@
+// src/main/frontend/src/setProxy.js
+
+// const {
+//     createProxyMiddleware
+// } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//     app.use(
+//         '/api',
+//         createProxyMiddleware({
+//             target: 'http://localhost:8080',
+//             changeOrigin: true,
+//         })
+//     );
+// };
