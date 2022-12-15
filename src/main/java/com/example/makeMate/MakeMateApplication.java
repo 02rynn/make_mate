3f32524d10d5b1c4fcc845182c7296408e175447
Charm-Star <zxzx05050@naver.com>
2022-12-15 14:52:15 +0900

    첫 번째 푸쉬

---
+++ src/main/java/com/example/makeMate/MakeMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMateApplication.class, args);
+	}
+
+}
