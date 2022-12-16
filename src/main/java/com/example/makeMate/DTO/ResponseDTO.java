61d6fdcd3aec564ac3e05b5da2ddc0452ae36079
MJ <lmj7772@gmail.com>
2022-12-16 12:27:16 +0900

    확인용 커밋

---
+++ src/main/java/com/example/makeMate/DTO/ResponseDTO.java
@@ -0,0 +1,24 @@
+package com.example.makeMate.DTO;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+
+
+public class ResponseDTO<T> {
+
+	private String error;
+	private List<T> data;
+	
+	
+	
+}
---
+++ src/main/java/com/example/makeMate/service/boardService.java
@@ -0,0 +1,22 @@
+package com.example.makeMate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.makeMate.Repository.BoardRepository;
+
+@Service
+
+public class boardService {
+
+	
+	@Autowired
+	public BoardRepository repository;
+	
+	
+	
+	
+	
+	
+	
+}
