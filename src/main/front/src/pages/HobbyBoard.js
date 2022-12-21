06726f9d6e78239b6621e52e827558f1feebf162
MJ <lmj7772@gmail.com>
2022-12-21 10:11:45 +0900

    커밋

--- src/main/front/src/pages/HobbyBoard.js
+++ src/main/front/src/pages/HobbyBoard.js
@@ -5,7 +5,6 @@ import {call} from "../service/ApiService";
 import {Paper, List, Container} from "@material-ui/core";
 
 class HobbyBoard extends React.Component {
-<<<<<<< HEAD
 	constructor(props) {
 	  super(props);
 	  this.state = {
@@ -63,65 +62,7 @@ class HobbyBoard extends React.Component {
 		</div>
 	  );
 	}
-=======
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-    };
->>>>>>> 7711a1ab8edf13025d0d0df6088ddb94fa277dc7
-  }
-
-  componentDidMount() {
-    call("/board", "GET", null).then((response) =>
-      this.setState({items: response.data})
-    );
-  }
-
-  add = (item) => {
-    call("/board", "POST", item).then((response) =>
-      this.setState({items: response.data})
-    );
-  };
-
-  delete = (item) => {
-    call("/board", "DELETE", item).then((response) =>
-      this.setState({items: response.data})
-    );
-  };
-
-  update = (item) => {
-    call("/board", "PUT", item).then((response) =>
-      this.setState({items: response.data})
-    );
-  };
-
-  render() {
-    var boardItems = this.state.items.length > 0 && (
-      <Paper style={{margin: 16}}>
-        <List>
-          {this.state.items.map((item, idx) => (
-            <Board
-              item={item}
-              key={item.id}
-              delete={this.delete}
-              update={this.update}
-            />
-          ))}
-        </List>
-      </Paper>
-    );
-
-    // 3. props로 넘겨주기
-    return (
-      <div className="App">
-        <Container maxWidth="md">
-          <AddBoard add={this.add} />
-          <div className="BoardList">{boardItems}</div>
-        </Container>
-      </div>
-    );
-  }
+  
 }
 
 export default HobbyBoard;
