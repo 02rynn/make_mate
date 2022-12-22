2bb2ede7f74b485a0c78ea356878f9282535bb2c
ljhmd00 <lmsmd00@naver.com>
2022-12-22 11:17:10 +0900

    Main.js(수정)

--- src/main/front/src/pages/Main.js
+++ src/main/front/src/pages/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Layout, theme, Card } from 'antd';
 import { useState, useEffect } from 'react';
-import MapContainer from '../components/MapContainer';
-import ProfileImgContainer from '../components/ProfileImgContainer';
+// import MapContainer from '../components/MapContainer';
+// import ProfileImgContainer from '../components/ProfileImgContainer';
+import user from '../images/user.jpg';
+import {useNavigate} from "react-router-dom";
+
 
 function Main() {
+  let navigate = useNavigate();
   const {Header, Content, Footer, Sider} = Layout;
   const {
     token: {colorBgContainer},
@@ -55,7 +59,6 @@ function Main() {
               console.log(collapsed, type);
             }}>
             <Card
-              hoverable
               style={{
                 width: "auto ",
                 margin: "3%",
@@ -63,15 +66,37 @@ function Main() {
               }}
               cover={
                 <img
+                  style={{padding:'3%',
+                          borderRadius:'10%'
+                }}
                   alt="example"
-                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  src={user}
                 />
               }>
               <Meta
                 style={{textAlign: "center"}}
                 title="유저닉네임"
-                description="www.instagram.com"
-              />
+                /><br/>
+                <p>유저 아이디</p>
+                <span style={{margin:'6%',
+                              backgroundColor: '#ff7f27',
+                              color: '#FFFFFF',
+                              padding:'1%',
+                              borderRadius:'10%'
+                
+                  }} 
+                      onClick={() => {
+                      navigate("mypage");
+                }}><a>내정보</a></span>
+                <span style={{margin:'6%',
+                              backgroundColor: '#ff7f27',
+                              color: '#FFFFFF',
+                              padding:'1%',
+                              borderRadius:'10%'
+              }} 
+                      onClick={() => {
+                      navigate("/");
+                }}><a>로그아웃</a></span>
             </Card>
           </Sider>
           <Layout>
@@ -97,8 +122,8 @@ function Main() {
                   minHeight: 1600,
                   background: colorBgContainer,
                 }}>
-                <div className='주혁개발공간'>
-                  <MapContainer/><ProfileImgContainer/>
+                <div>
+                  게시판 Map 불러오기
                 </div>
               </div>
             </Content>
@@ -106,7 +131,7 @@ function Main() {
               style={{
                 textAlign: "center",
               }}>
-              Ant Design ©2018 Created by Ant UED
+              Human Design ©2022 Created by TDOUCI5
             </Footer>
           </Layout>
         </Layout>
