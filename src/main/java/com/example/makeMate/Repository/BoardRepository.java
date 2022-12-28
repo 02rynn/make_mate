6472ec325df4b31f41dea23db133130e8eb9c7d6
Charm-Star <zxzx05050@naver.com>
2022-12-28 11:50:20 +0900

    properties추가 설정

--- src/main/java/com/example/makeMate/Repository/BoardRepository.java
+++ src/main/java/com/example/makeMate/Repository/BoardRepository.java
@@ -10,7 +10,19 @@ import com.example.makeMate.Entity.Board;
 
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+	
+	
+	// select * from board order desc;
+	List<Board> finaAllOrderByIdDesc();
+	
+	//insert into board(board_idx, board_name, board_ttl, board_cn, board_date)
+   // values(nextval('board_seq'), #{param1}, #{param2}, #{param3}, now())
 
+	Board save(Board board);
+	
+	
+	
+	
 
 
 }
