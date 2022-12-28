a1ce95a58e2d212ce5f0fb92b016eeb2e0ace8a6
Charm-Star <zxzx05050@naver.com>
2022-12-28 16:56:12 +0900

    이미지 경로 저장 및 사진 저장 추가

--- src/main/java/com/example/makeMate/Entity/UserEntity.java
+++ src/main/java/com/example/makeMate/Entity/UserEntity.java
@@ -1,16 +1,16 @@
 package com.example.makeMate.Entity;
 
 import java.sql.Date;
-import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -25,6 +25,8 @@ public class UserEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator ="user_seq")
 	@SequenceGenerator(name = "user_seq",sequenceName = "USER_SEQ",initialValue = 1, allocationSize = 1)
+//	@OneToOne
+//	@JoinColumn(name = "id")
 	private long id;
 	
 	@Column(name = "email", nullable = false)
--- src/main/java/com/example/makeMate/Repository/BoardRepository.java
+++ src/main/java/com/example/makeMate/Repository/BoardRepository.java
@@ -1,20 +1,20 @@
-package com.example.makeMate.Repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.makeMate.Entity.BoardEntity;
-
-
-
-@Repository
-public interface BoardRepository extends JpaRepository<BoardEntity, String>{
-	List<BoardEntity> findByUserId(String userId);
-	
-	
-
-	
-	
-}
+//package com.example.makeMate.Repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.example.makeMate.Entity.BoardEntity;
+//
+//
+//
+//@Repository
+//public interface BoardRepository extends JpaRepository<BoardEntity, String>{
+//	List<BoardEntity> findByUserId(String userId);
+//	
+//	
+//
+//	
+//	
+//}
