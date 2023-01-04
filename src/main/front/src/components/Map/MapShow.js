6622402a7b0546f5116ed9e14b31d7cd0f2035cd
ljhmd00 <lmsmd00@naver.com>
2023-01-04 19:39:41 +0900

    Map폴더생성

--- src/main/front/src/components/Map/MapShow.js
+++ src/main/front/src/components/Map/MapShow.js
@@ -3,11 +3,11 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const { kakao } = window;
 
-function MapSearch() {
+function MapShow() {
 
     const [state, setState] = useState({
   // 지도의 초기 위치
-    center: { lat: 37.49676871972202, lng: 127.02474726969814 },
+    center: { lat: 36.807276, lng: 127.147177},
   // 지도 위치 변경시 panto를 이용할지(부드럽게 이동)
     isPanto: true,
 });
@@ -16,7 +16,7 @@ const [searchAddress, SetSearchAddress] = useState();
 // 키워드 입력후 검색 클릭 시 원하는 키워드의 주소로 이동
 const SearchMap = () => {
     const ps = new kakao.maps.services.Places()
-    const placesSearchCB = function(data, status, pagination) {
+    const placesSearchCB = function(data, status) {
     if (status === kakao.maps.services.Status.OK) {
         const newSearch = data[0]
         setState({
@@ -47,12 +47,8 @@ const SearchMap = () => {
                     <div>{searchAddress}</div> 
                 </MapMarker>
                 </Map>
-                <div>
-                    <input style={{border:'1px solid orange'}} onChange={handleSearchAddress}></input>
-                    <button  onClick={SearchMap}  style={{backgroundColor:'orange'}}>검색</button>
-                </div>
             </div>
     );
 }
 
-export default MapSearch; 
+export default MapShow; 
---
+++ src/main/front/src/components/Map/Test.js
@@ -0,0 +1,13 @@
+import MapShow from "./MapShow";
+import MapSearch from "./MapSearch";
+
+function test () {
+    return(
+        <div>
+            <MapSearch/>
+            <br/>
+            <MapShow/>
+        </div>
+    )
+}
+export default test;
