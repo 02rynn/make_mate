35c079f7f91b0c53431e44d0fdab5406df57d9f3
MJ <lmj7772@gmail.com>
2023-01-10 15:50:58 +0900

    제발

--- src/main/front/src/components/Admin/Admin.js
+++ src/main/front/src/components/Admin/Admin.js
@@ -1,41 +1,34 @@
 
-
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
+import  './Admin.css';
 
 
 
-
-class BbsList extends Component {
-   
-	
-	
-	
-	render() {
+class Admin extends Component {
+    render() {
         return (
             <div className="admin-menu-container">
                 <h2>관리자님, 환영합니다</h2>
                 <Button type="primary" block size="large">
-                   
-                        게시판 추가
-                   
+                    <Link to="/newAdd">
+                      게시판 추가
+                    </Link>
                 </Button>
                 <Button type="primary" block size="large">
-                   
-                    게시판삭제
-                    
+                    <Link to="/newEpi">
+                    게시판 삭제
+                    </Link>
                 </Button>
                 <Button type="primary" block size="large">
-                    
+                    <Link to="/AdminCustomer">
                     회원정지
-                 
-                    </Button>
+                    </Link>
+                </Button>
             </div>
         );
     }
 }
 
-
-
-export default BbsList;
+export default Admin;
