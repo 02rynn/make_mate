54629715f1d366c1ae767c66d0ba50feffa73092
ljhmd00 <lmsmd00@naver.com>
2023-01-11 16:13:14 +0900

    Review.js(css 수정)

--- src/main/front/src/pages/Review.js
+++ src/main/front/src/pages/Review.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react';
 import { FaStar } from "react-icons/fa";
+import Card from 'react-bootstrap/Card';
+
 
 const colors= {
     orange: "#FFBA5A",
@@ -24,23 +26,6 @@ const submit=()=>{
 
 
 function Review() {
-    const selectComponent = {
-        list: [
-          {name1: "second", time: "22/12/18 23:12"},
-          {name1: "third", time: "22/12/18 18:12"},
-          {name1: "fourth", time: "22/12/18 34:12"},
-          {name1: "fifasdth1", time: "22/12/18 03:12"},
-          {name1: "fifasdasth2", time: "22/12/18 04:12"},
-          {name1: "fifthasd3", time: "22/12/18 05:12"},
-          {name1: "fiftdh4", time: "22/12/18 14:12"},
-          {name1: "fiftash5", time: "22/12/18 09:12"},
-          {name1: "fiftdasdh6", time: "22/12/18 11:12"},
-          {name1: "fifasdth7", time: "22/12/18 23:32"},
-          {name1: "fiftasdh8", time: "22/12/18 22:12"},
-        ],
-      };
-    
-
     
     const stars = Array(5).fill(0);
     const [currentValue, setCurrentValue] = React.useState(0);
@@ -56,41 +41,46 @@ function Review() {
 
     return(
 
-        <div> 
-            <div className='reviewContainer' style={styles.container}>
-                <h2 style={{left:'20px'}}>리뷰 페이지</h2>
-                <div style={styles.stars}>
-                    {stars.map((_,index)=>{
-                        return(
-                            <FaStar
-                                key={index}
-                                size={24}
-                                style={{
-                                    marginRight:10,
-                                    cursor:"pointer"
-                                }}
-                                color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
-                                onClick={() => handleClick(index + 1)}
-                                
-                            />
-                        )
-                    })}
-                </div>
-                <textarea
-                    placeholder='리뷰를 남겨주세요.'
-                    style={styles.textarea}
-                />
-                <button style={styles.button} onClick={()=>{
-                    submit()
-                }}>Submit</button>
-            </div>
+        <div style={{border:'20px solid #001529', marginTop:'5%', borderRadius:10}}> 
+            <Card style={{minHeight:'500px', border:'10px solid #FF7F27', borderRadius:0}}>
+                <Card.Title>
+                        <h2 style={{marginTop:'5%'}}>REVIEW</h2>
+                        <div style={styles.stars}>
+                            {stars.map((_,index)=>{
+                                return(
+                                    <FaStar
+                                        key={index}
+                                        size={24}
+                                        style={{
+                                            marginRight:10,
+                                            cursor:"pointer"
+                                        }}
+                                        color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+                                        onClick={() => handleClick(index + 1)}
+                                        
+                                    />
+                                )
+                            })}
+                        </div>
+                </Card.Title>
+                <Card.Body>
+                    <div className='reviewContainer' style={styles.container}>
+                        <textarea
+                            placeholder='리뷰를 남겨주세요.'
+                            style={styles.textarea}
+                        />
+                        <button style={styles.button} onClick={()=>{
+                            submit()
+                        }}>리뷰남기기</button>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
 
 const styles = {
     container: {
-        display:"flex",
         flexDirection:"column",
         aliItems:"center",
         margin: "0 33% 0 33%",
@@ -98,18 +88,18 @@ const styles = {
         
     },
     textarea: {
-        border:'1px solid #a9a9a9',
+        border:'1px solid #001529',
         borderRadius: 5,
-        width: 300,
-        margin: "20px 0",
-        minHeight: 100,
+        width:'100%',
+        minHeight: 200,
         padding: 10
     },
     button: {
-        border:'1px solid #a9a9a9',
+        border:'2px solid #001529',
         borderRadius: 5,
-        width: 300,
-        padding: 10
+        width: '100%',
+        padding: 10,
+        backgroundColor:'#FF7F27',
     }
 }
 
