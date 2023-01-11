c2a22982cd894b9a61b9cb98431a83ffc055a38d
ljhmd00 <lmsmd00@naver.com>
2023-01-11 15:04:08 +0900

    Note.js(css수정), MsgModal.js(css수정)

--- src/main/front/src/components/MsgModal.js
+++ src/main/front/src/components/MsgModal.js
@@ -2,6 +2,9 @@ import "../App.css";
 import axios from "axios";
 import {useState, useEffect} from "react";
 import SockJS from "sockjs-client";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 function MsgModal(props) {
   const user = sessionStorage.getItem("loginId");
@@ -17,68 +20,98 @@ function MsgModal(props) {
           className="msgModalBox"
           action="/msg"
           method="post"
-          name="msgModalBox">
-          <p> 쪽지 보내기 </p>
-          <input type="text" id="reciver_id" value={props.tab}></input>
-          <a
-            style={{
-              float: "right",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              props.setModal(false);
-            }}>
-            닫기{" "}
-          </a>{" "}
-          <textarea
-            name="message"
-            className="message"
-            placeholder="내용을 입력해주세요."
-            id="msgbox"
-            value={props.userData.message}
-            onChange={props.handleMessage}
-            onKeyDown={(e) => {
-              if (e.keyCode === 13) {
-                // axios
-                //   .post("/msg", {
-                //     message,
-                //   })
-                //   .then(() => {
-                //     console.log("success");
-                //   })
-                //   .catch(() => {
-                //     console.log("fail");
-                props.sendPrivateValue();
+          name="msgModalBox"
+          style={{border:'1px solid #001529',borderRadius:'5%'}}
+          >
+            <Card style={{border:'1px solid #001529'}}>
+              <Card.Title style={{fontWeight:'700', marginTop:'4%'}}> 
+                쪽지 보내기
+              </Card.Title>
+              <Card.Body>
+                <input type="text" 
+                        id="reciver_id" 
+                        value={props.tab}
+                        style={{textAlign:'center'}}
+                />
+                <textarea
+                    style={{width:'100%', 
+                            marginLeft:"0",
+                            border:'1px solid #001529',
+                            minHeight:'130px'
+                          }}    
+                    name="message"
+                    className="message"
+                    placeholder="내용을 입력해주세요."
+                    id="msgbox"
+                    value={props.userData.message}
+                    onChange={props.handleMessage}
+                    onKeyDown={(e) => {
+                      if (e.keyCode === 13) {
+                        // axios
+                        //   .post("/msg", {
+                        //     message,
+                        //   })
+                        //   .then(() => {
+                        //     console.log("success");
+                        //   })
+                        //   .catch(() => {
+                        //     console.log("fail");
+                        props.sendPrivateValue();
 
-                props.setModal(false);
-              }
+                        props.setModal(false);
+                      }
 
-              console.log(e.target.value);
-              // console.log(e);
-            }}></textarea>
-          <input
-            value="전송"
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              //전송
-              // axios
-              //   .post("/msg", {
-              //     message,
-              //   })
-              //   .then(() => {
-              //     console.log("success");
-              //   })
-              //   .catch(() => {
-              //     console.log("fail");
-              //   });
-              props.sendPrivateValue();
+                      console.log(e.target.value);
+                      // console.log(e);
+                    }}>
+                  </textarea>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant="outline-primary"
+                  style={{backgroundColor:' #FF7F27',
+                          color:'white', 
+                          border:'1px solid  #FF7F27',
+                          marginLeft:'53%'
+                        }}
+                  value="전송"
+                  type="submit"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    //전송
+                    // axios
+                    //   .post("/msg", {
+                    //     message,
+                    //   })
+                    //   .then(() => {
+                    //     console.log("success");
+                    //   })
+                    //   .catch(() => {
+                    //     console.log("fail");
+                    //   });
+                    props.sendPrivateValue();
 
-              props.setModal(false);
+                    props.setModal(false);
 
-              // document.form.msgModalBox.submit();
-            }}
-          />{" "}
+                    // document.form.msgModalBox.submit();
+                }}>
+                  전송
+                </Button>
+                <Button variant="outline-primary"
+                  style={{
+                    float: "right",
+                    cursor: "pointer",
+                    backgroundColor:'#001529',
+                    color:'white', 
+                    border:'1px solid #001529'
+                  }}
+                    onClick={() => {
+                    props.setModal(false);
+                  }}
+                >
+                  닫기
+                </Button>                
+              </Card.Footer>
+            </Card>
         </form>{" "}
       </div>
     </div>
