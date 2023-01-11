d3b0f061d2ab421d4cb98c31c2aa57b4b99a3528
o2rynn <flsgp123@naver.com>
2023-01-11 16:06:45 +0900

    리뷰 기능 구현시작

--- src/main/front/src/pages/Review.js
+++ src/main/front/src/pages/Review.js
@@ -1,68 +1,74 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaStar } from "react-icons/fa";
 
-const colors= {
-    orange: "#FFBA5A",
-    grey:"#a9a9a9"
-}
-
-const submit=()=>{
-    axios({
-        method: "post",
-        url: "http://localhost:8080/review",
-        // data: values,
-    })
-    .catch((e) => {
-        console.error(e.response.data);
-    })
-    .then((response) =>{
-        console.log(response);
-    })
-}
 
 
 
 function Review() {
-    const selectComponent = {
-        list: [
-          {name1: "second", time: "22/12/18 23:12"},
-          {name1: "third", time: "22/12/18 18:12"},
-          {name1: "fourth", time: "22/12/18 34:12"},
-          {name1: "fifasdth1", time: "22/12/18 03:12"},
-          {name1: "fifasdasth2", time: "22/12/18 04:12"},
-          {name1: "fifthasd3", time: "22/12/18 05:12"},
-          {name1: "fiftdh4", time: "22/12/18 14:12"},
-          {name1: "fiftash5", time: "22/12/18 09:12"},
-          {name1: "fiftdasdh6", time: "22/12/18 11:12"},
-          {name1: "fifasdth7", time: "22/12/18 23:32"},
-          {name1: "fiftasdh8", time: "22/12/18 22:12"},
-        ],
-      };
+    // const selectComponent = {
+    //     list: [
+    //       {name1: "second", time: "22/12/18 23:12"},
+    //       {name1: "third", time: "22/12/18 18:12"},
+    //       {name1: "fourth", time: "22/12/18 34:12"},
+    //       {name1: "fifasdth1", time: "22/12/18 03:12"},
+    //       {name1: "fifasdasth2", time: "22/12/18 04:12"},
+    //       {name1: "fifthasd3", time: "22/12/18 05:12"},
+    //       {name1: "fiftdh4", time: "22/12/18 14:12"},
+    //       {name1: "fiftash5", time: "22/12/18 09:12"},
+    //       {name1: "fiftdasdh6", time: "22/12/18 11:12"},
+    //       {name1: "fifasdth7", time: "22/12/18 23:32"},
+    //       {name1: "fiftasdh8", time: "22/12/18 22:12"},
+    //     ],
+    //   };
     
-
+    const colors= {
+        orange: "#FFBA5A",
+        grey:"#a9a9a9"
+    }
+    
+    const submit=()=>{
+        axios({
+            method: "post",
+            url: "http://localhost:8080/review",
+            data: review,
+        })
+        .catch((e) => {
+            console.error(e.response.data);
+        })
+        .then((response) =>{
+            console.log(response);
+        })
+    }
+    
     
     const stars = Array(5).fill(0);
     const [currentValue, setCurrentValue] = React.useState(0);
     const [hoverValue, setHoverValue] = React.useState(undefined);
+    const [reviewValue, setReviewValue] = useState("");
+    const review = [...reviewValue];
 
     const handleClick = value => {
         setCurrentValue(value)
+        console.log(currentValue);
     };
 
     const handleMouseOver =() => {
         setHoverValue(undefined)
     }
 
+    
     return(
 
         <div> 
+            <form>
             <div className='reviewContainer' style={styles.container}>
                 <h2 style={{left:'20px'}}>리뷰 페이지</h2>
                 <div style={styles.stars}>
                     {stars.map((_,index)=>{
                         return(
                             <FaStar
+                                name='rate'
                                 key={index}
                                 size={24}
                                 style={{
@@ -77,13 +83,16 @@ function Review() {
                     })}
                 </div>
                 <textarea
+                    name='reviewContent'
                     placeholder='리뷰를 남겨주세요.'
                     style={styles.textarea}
                 />
-                <button style={styles.button} onClick={()=>{
+                <button  style={styles.button} onClick={(e)=>{
                     submit()
+                    setReviewValue(e.target.value)
                 }}>Submit</button>
             </div>
+            </form>
         </div>
     )
 }
